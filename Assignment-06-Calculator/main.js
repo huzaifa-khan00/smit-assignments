d9dfaaf4e243890a7c01f109974944880176dc90
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-18 04:14:48 +0500

    Validate empty evaluation

--- Assignment-06-Calculator/main.js
+++ Assignment-06-Calculator/main.js
@@ -1,5 +1,4 @@
 let equalPress = 0;
-let shiftValue;
 let tempArr = [];
 let errorDisplay = document.createElement('p');
 let prohibCharsRight = ['+', '-', '/', '*', '(', '.'];
@@ -10,44 +9,52 @@ function calculator(data) {
 
   tempArr.unshift(data);
 
-  if(tempArr.length >= 2){
-     if(illegaChars.includes(tempArr[0] + tempArr[1]) ){
-        let currentData = document.getElementById('current-display');
+  if (tempArr.length >= 2){
+     if (illegaChars.includes( tempArr[0] + tempArr[1])) {
+        let currentData = document.getElementById( 'current-display' );
           currentData.innerText = currentData.innerText.slice(0, currentData.innerText.length-1);
         errorDisplay.id = 'error-display2';
-        document.getElementsByClassName('btn-container')[0].prepend(errorDisplay);
+        document.getElementsByClassName( 'btn-container' )[0].prepend(errorDisplay);
         errorDisplay.innerText = "Double " + tempArr[0] + tempArr[1] + " Are Illegal";
         tempArr = [];
         return;
   }
   }
 
- 
+  if(document.getElementById('current-display').innerText == '' && data == 'equal'){
+     let currentData = document.getElementById( 'current-display' );
+     errorDisplay.id = 'error-display2';
+     document.getElementsByClassName( 'btn-container' )[0].prepend(errorDisplay);
+     errorDisplay.innerText = "Please input value";
+     return;
+  }
+  errorDisplay.innerText = "";
+  
 
   if (data == "equal") {
     equalPress++;
 
-    let currentData = document.getElementById("current-display").innerText;
+    let currentData = document.getElementById( "current-display" ).innerText;
 
-    let secValue = document.getElementById("second-display").innerText;
+    let secValue = document.getElementById( "second-display" ).innerText;
 
-    document.getElementById("second-display").innerText = currentData;
+    document.getElementById( "second-display" ).innerText = currentData;
 
     if (equalPress >= 1) {
 
       if (prohibCharsRight.includes(currentData.slice(-1)) || prohibCharsLeft.includes(currentData.slice(0, 1))) {
         errorDisplay.id = 'error-display';
-        document.getElementsByClassName('btn-container')[0].prepend(errorDisplay);
+        document.getElementsByClassName( 'btn-container' )[0].prepend(errorDisplay);
         errorDisplay.innerText = " Invalid Input";
         return;
       }
 
       if (equalPress >= 2) {
-        document.getElementById("third-display").innerText = secValue;
-        document.getElementById("current-display").innerText = currentData;
+        document.getElementById( "third-display" ).innerText = secValue;
+        document.getElementById( "current-display" ).innerText = currentData;
       }
 
-      currentData = document.getElementById("current-display").innerText = eval(currentData);
+      currentData = document.getElementById( "current-display" ).innerText = eval( currentData );
 
       return;
     }
@@ -55,23 +62,23 @@ function calculator(data) {
 
   if (data == "clear") {
     equalCount = 0;
-    document.getElementById("current-display").innerText = "";
-    document.getElementById("second-display").innerText = "";
-    document.getElementById("third-display").innerText = "";
-    if(errorDisplay.innerText != ''){
-      errorDisplay.innerText = '';
+    document.getElementById( "current-display" ).innerText = "";
+    document.getElementById( "second-display" ).innerText = "";
+    document.getElementById( "third-display" ).innerText = "";
+    if (errorDisplay.innerText != '') {
+        errorDisplay.innerText = '';
     }
     return;
   }
 
   if (data == "del") {
-    let del = document.getElementById("current-display").innerText;
+    let del = document.getElementById( "current-display" ).innerText;
     del = del.slice(0, del.length - 1);
-    document.getElementById("current-display").innerText = del;
+    document.getElementById( "current-display" ).innerText = del;
     return;
   }
 
-  result = document.getElementById("current-display");
+  result = document.getElementById( "current-display" );
   
   result.innerText += data;
   }
